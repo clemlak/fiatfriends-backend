10a1764b043b2b199a9c2760198985492b11a9f7
Anurag <anurag.angara@gmail.com>
2019-03-09 21:17:27 +0100

    minor reconfig

--- fiatFriendsEthStack/App.js
+++ fiatFriendsEthStack/App.js
@@ -6,7 +6,7 @@ const swapCallRinkeby = require('./swapCallRinkeby.js');
 
 let recipientAddress = '0xfd14676d15E5ce66b29C18016cAa2Dd146f9c00e';
 let amount = '0.01';
-let ticker = 'DA';
+let ticker = 'DAI';
 let currencyFrom = "USD";
 
 //call hotswap
--- fiatFriendsEthStack/data/ethersConfig.js
+++ fiatFriendsEthStack/data/ethersConfig.js
@@ -1,9 +1,9 @@
 const ethers = require('ethers');
 
-const apiAccessToken = 'c00073b64f4d41bf9c7dbc342472d90c';
-const defaultProvider = ethers.getDefaultProvider("rinkeby");
+const apiAccessToken = '';
+const defaultProvider = ethers.getDefaultProvider("homestead");
 
-const privateKey = '4BEF4E919D0AEF735882F465734FD47BDC1376F11E2D2B626BEE1308198B7D4E';
+const privateKey = '';
 const wallet = new ethers.Wallet(privateKey, defaultProvider);
 module.exports = {
     wallet: wallet
