bdc904e77d3dcb81bb396e0e2bc8feec766322a6
Anurag <anurag.angara@gmail.com>
2019-03-09 19:49:51 +0100

    eth fix

--- fiatFriendsEthStack/App.js
+++ fiatFriendsEthStack/App.js
@@ -6,7 +6,7 @@ const swapCallRinkeby = require('./swapCallRinkeby.js');
 
 let recipientAddress = '0xfd14676d15E5ce66b29C18016cAa2Dd146f9c00e';
 let amount = '0.01';
-let ticker = 'DAI';
+let ticker = 'DbI';
 console.log(amount);
 //call hotswap
 swapCallRinkeby.swap(recipientAddress, amount, ticker);
--- fiatFriendsEthStack/swapLogic/ethDirectSend.js
+++ fiatFriendsEthStack/swapLogic/ethDirectSend.js
@@ -7,9 +7,8 @@ module.exports = {
 
 
 async function sendEth(recipientAddress, amount){
-    console.log(recipientAddress)
     let transaction = {
-        gasLimit: 100000,
+
         to: recipientAddress,
         // ... or supports ENS names
         // to: "ricmoo.firefly.eth",
@@ -17,7 +16,7 @@ async function sendEth(recipientAddress, amount){
         value: ethers.utils.parseEther(amount.toString()),
     
         // This ensures the transaction cannot be replayed on different networks
-        chainId: ethers.utils.getNetwork('homestead').chainId
+
     }
     return ethersConfig.wallet.sendTransaction(transaction);
 }
