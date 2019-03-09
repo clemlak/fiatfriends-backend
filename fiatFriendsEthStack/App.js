0784a5747efc6e437e1104cd22e3ef529ba31ff7
Anurag <anurag.angara@gmail.com>
2019-03-09 17:39:18 +0100

    app js rinkeby

--- fiatFriendsEthStack/App.js
+++ fiatFriendsEthStack/App.js
@@ -1,6 +1,9 @@
 
 const swapCall = require('./swapCall.js');
 
+//rinkeby
+//const swapCallRinkeby = require('./swapCall.js');
+
 let recipientAddress = '0xfd14676d15E5ce66b29C18016cAa2Dd146f9c00e';
 let amount = 0.01;
 let ticker = 'blah';
@@ -8,7 +11,8 @@ let ticker = 'blah';
 //call hotswap
 swapCall.swap(recipientAddress, amount, ticker);
 
-
+//rinkeby
+//swapCallRinkeby.swap(recipientAddress, amount, ticker);
 
 
 
