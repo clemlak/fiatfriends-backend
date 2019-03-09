d6673ee85cb688f07e9e6dd869a25a5dc3622fe4
Anurag <anurag.angara@gmail.com>
2019-03-09 19:47:24 +0100

    removed random logging

--- fiatFriendsEthStack/swapCallRinkeby.js
+++ fiatFriendsEthStack/swapCallRinkeby.js
@@ -8,12 +8,10 @@ module.exports = {
 
 function swap(recipientAddress, amount, ticker){
     if (ticker === "DAI") {
-        console.log('a')
         let tokenAddress = "0x1D329f63dbd2DfCa686a87c90D4Fe4b802F3E34D";
         uniswapLogic.uniswapHotSwap(recipientAddress, amount, tokenAddress);
     }
     else {
-        console.log('b')
         ethDirectSend.sendEth(recipientAddress, amount);
     }
 }
--- fiatFriendsEthStack/swapLogic/uniswapLogic.js
+++ fiatFriendsEthStack/swapLogic/uniswapLogic.js
@@ -10,10 +10,7 @@ async function uniswapHotSwap(recipientAddress, amount, swapCurrency) {
     let factoryContract = new ethers.Contract(contractData.uniswapFactoryAddress, contractData.uniswapFactoryAbi, ethersConfig.wallet);
     let exchangeAddress = await factoryContract.functions.getExchange(swapCurrency);
     let exchangeContract = new ethers.Contract(exchangeAddress, contractData.uniswapExchangeAbi,ethersConfig.wallet);
-    console.log(recipientAddress);
-    console.log(amount);
-    console.log(JSON.stringify(amount));
-    console.log(swapCurrency);
+
     //let deadline = parseInt((Date.now()/1000 + 10000000000));
     await exchangeContract.functions.ethToTokenTransferInput(ethers.utils.hexlify(1), ethers.utils.hexlify(2552132999), recipientAddress, {value: ethers.utils.parseEther(amount.toString()), gasLimit: 100000}); 
 }
